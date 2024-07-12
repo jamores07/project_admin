a9e217c9a194df5bae0dab4a47640ec8e60dbcc8
John Amores <kayak.stated_0b@icloud.com>
2024-07-12 09:52:20 -0400

    Added auth components; WIP

---
+++ components/auth/AuthTabs.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const AuthTabs = (props: Props) => {
+  return (
+    <div>AuthTabs</div>
+  )
+}
+
+export default AuthTabs
---
+++ components/auth/RegisterForm.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import BackButton from '@/components/BackButton';
+import * as z from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import { useRouter } from 'next/navigation';
+
+
+const RegisterForm = () => {
+  return (
+    <div>RegisterForm</div>
+  )
+}
+
+export default RegisterForm
